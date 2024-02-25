370155a82223bba513eda04887e73b5e2ed74dc4
Darialucky <144655337+Darialucky@users.noreply.github.com>
2024-02-25 17:00:31 +0200

    +++

---
+++ src/js/refs.js
@@ -0,0 +1,9 @@
+export const refs = {
+    form: document.querySelector('.user-form'),
+    formInput: document.querySelector('.form-input'),
+    btn: document.querySelector('.form-button'),
+    galleryList: document.querySelector('.gallery'),
+    container: document.querySelector('.container'),
+    loader: document.querySelector('.loader'),
+    btnLoad: document.querySelector('.btn-load'),
+  };
